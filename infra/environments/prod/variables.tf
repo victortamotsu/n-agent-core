5eb8908327a5570be271a1345ad82a19e394c936
Victor Tamotsu <victor@incubemetrics.com>
2025-12-27 16:33:31 -0300

    feat: add terraform config for prod environment

---
+++ infra/environments/prod/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "n-agent"
+}
